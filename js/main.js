b0e78fb6c92b5761a00f9962aa2a5d90b541cb04
Nicolas Lound <nicolas.lound@rga.com>
2018-11-13 19:06:46 -0300

    Add sounds

--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
 window.onload = function () {
+
+
+
+var bongoHi = "BongoHi";
+var bongoLow = "BongoLow";
+createjs.Sound.registerSound("mp3/hi.mp3", bongoHi);
+createjs.Sound.registerSound("mp3/low.mp3", bongoLow);
+
+
   const bongos = document.getElementsByClassName('cat-bongo')[0];
 
 
@@ -27,13 +36,16 @@ window.onload = function () {
       case 'ArrowLeft':
         pawLeft.classList.add('hide');
         hitLeft.classList.add('hide');
+        createjs.Sound.play(bongoLow);
         break;
-      case 'ArrowRight':
+        case 'ArrowRight':
         pawRight.classList.add('hide');
         hitRight.classList.add('hide');
+        createjs.Sound.play(bongoHi);
         break;
     }
   });
 
 
 };
+
